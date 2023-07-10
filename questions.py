21437c0e40b362385b2931fe9aa545e9d2de4e2e
tcalmeida <tcalmeida85@gmail.com>
2023-07-10 14:06:08 -0300

    add questions list and answers tuple

---
+++ questions.py
@@ -0,0 +1,13 @@
+questions = {
+    'What country has the highest life expectancy?: ': 'A',
+    'What is the most common surname in the United States?: ': 'B',
+    'Who was the Ancient Greek God of the Sun?: ': 'C',
+    'What city is known as "The Eternal City"?: ': 'A',
+    'Which planet has the most moons? Saturn?: ': 'D'
+}
+
+answers = [('A. Hong Kong', 'B. Beijing', 'C. Vietnam', 'D. London'),
+           ('A. Wood', 'B. Smith', 'C. Doe', 'D. Gates'),
+           ('A. Venus', 'B. Mars', 'C. Apollo', 'D. Poseidon '),
+           ('A. Rome', 'B. New York', 'C. Paris', 'D. Barcelona'),
+           ('A. Mars', 'B. Jupiter', 'C. Uranus', 'D. Saturn')]

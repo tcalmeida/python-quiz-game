60a8eba7d979bb99789332f989efcb0601255b3c
tcalmeida <tcalmeida85@gmail.com>
2023-07-10 14:31:54 -0300

    refactor: change question_num value to zero

--- functions.py
+++ functions.py
@@ -4,12 +4,12 @@ import questions as q
 def new_game():
     player_answers = []
     correct_answer = 0
-    question_num = 1
+    question_num = 0
 
     for key in q.questions:
         print('----------------------------')
         print(key)
-        for i in q.answers[question_num - 1]:
+        for i in q.answers[question_num]:
             print(i)
         answer = input('Enter the correct question (A, B, C or D): ').upper()
         player_answers.append(answer)

7123f77e2f27ac531dc1238fe6095f18a12a2650
tcalmeida <tcalmeida85@gmail.com>
2023-07-10 14:15:11 -0300

    create check answer function

--- functions.py
+++ functions.py
@@ -13,3 +13,20 @@ def new_game():
             print(i)
         answer = input('Enter the correct question (A, B, C or D): ').upper()
         player_answers.append(answer)
+
+        # check correct answer and computes a point
+        correct_answer += check_answer(q.questions.get(key), answer)
+        question_num += 1
+
+    display_score(correct_answer, player_answers)
+
+
+def check_answer(question, answer):
+    if question == answer:
+        print('Correct!')
+        return 1
+    else:
+        print('Wrong!')
+        return 0
+
+

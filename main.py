b5a6d26ccbf6709e1b2813795b2513fb329f51ed
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-07-18 15:58:52 -0300

    refactor: add main function and if __name__ conditional

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from functions import new_game, play_again
 
-new_game()
 
-while play_again():
+def main():
     new_game()
 
-print('Thank you for playing!!!')
+    while play_again():
+        new_game()
+
+    print('Thank you for playing!!!')
+
+
+if __name__ == '__main__':
+    main()
